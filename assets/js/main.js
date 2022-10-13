d878d39840aa38c68c0c62e01a9a2c75fdf618ab
SimoneLollini <lollinisimone@gmail.com>
2022-10-13 15:12:14 +0200

    add list items

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,15 +10,13 @@ let i = 0;
 while (i < list.length) {
     // const liElement = document.createElement('li')
     const itemElement = list[i]
-    //const item = document.querySelector("ul").appendChild(itemElement)
-    const liElement = document.createElement("li");
-    const item = document.createTextNode(itemElement);
-    // liElement.appendChild(item);
-    const ulElement = document.querySelector('ul');
-    // list.insertBefore(liElement, ulElement.children[i]);
-    document.querySelector('ul').innerHTML = `<li>${itemElement}</li>`
     console.log(itemElement);
-    //.appendChild(itemElement)
+    // document.querySelector('ul').insertAdjacentHTML('beforeend', itemElement)
 
+    const ul = document.querySelector('ul')
+    const liElement = document.createElement("li")
+    ul.append(liElement)
+    liElement.append(itemElement)
     i++
 }
+
